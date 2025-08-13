9ebe67d51fdb76627960115937849bc0b2de5b2f
fmp9 <flo99p@gmx.at>
2025-08-13 12:44:18 +0200

    setup, navbar

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,23 @@
+import { Tabs } from "expo-router";
+import Feather from '@expo/vector-icons/Feather';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
+export default function TabsLayout() {
+    return <Tabs screenOptions={{tabBarActiveTintColor: "green"}}>
+        <Tabs.Screen name="index" options={{
+            title: "Home",
+            tabBarIcon: ({color}) => ( <MaterialCommunityIcons name="home-variant-outline" size={24} color={color} />),}}/>
+        <Tabs.Screen name="calendar" options={{
+            title: "Kalender",
+            tabBarIcon: ({color}) => ( <MaterialCommunityIcons name="calendar-month" size={24} color={color} />),}}/>
+
+        <Tabs.Screen name="training" options={{
+            title: "Training",
+            tabBarIcon: ({color}) => ( <MaterialCommunityIcons name="arm-flex-outline" size={24} color={color} />),}}/>
+
+        <Tabs.Screen name="profile" options={{
+            title: "Profil",
+            tabBarIcon: ({color}) => ( <MaterialCommunityIcons name="face-man" size={24} color={color} />),}}/>
+
+    </Tabs>
+}
---
+++ app/(tabs)/training.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Training() {
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text>Training</Text>
+        </View>
+    );
+}
